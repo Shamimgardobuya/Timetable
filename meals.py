5bfa5cdc760e113b86cc7da9252f2a50cfab98c3
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-27 23:09:00 +0300

    did iteration

--- meals.py
+++ meals.py
@@ -1,14 +1,16 @@
-from time import time
+# from time import time
 
 
 Monday={"Breakfast":"Mandazi","Lunch":"Fish","Supper":"Chapatti"}
 Tuesday={"Breakfast":"Bread","Lunch":"Potatoes","Supper":"Ugali"}
 Wednesday={"Breakfast":"Sausage with Tea","Lunch":"Rice","Supper":"Chicken"}
-
+day=input("Enter the day:")
 time=input("Enter the meal time: ")
-day=input("Enter the day: ")
-if day=="Monday":
-        print(Monday)
-elif day=="Tuesday":
-        print(Tuesday.get("Breakfast"))
+time=day.values()
+day = Monday or Tuesday or Wednesday
+
+if day=="Monday" or Tuesday or Wednesday :
+        print(day and time)
+# elif day=="Tuesday":
+#         print(Tuesday.get("Breakfast"))
         
